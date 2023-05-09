42dfc975867f5366f2b520fc388346edd555eec8
Emanuel_AkumaA <emmanuel.akuma.dev@gmail.com_AkumaA>
2023-05-09 17:02:51 -0300

    DB Gerenciador de tarefas

---
+++ Módulo_2 (Banco de Dados)/Masterclass_Gileno_Atividade_2/M2S2_Gerenciamento_de_tarefas.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conexao = sqlite3.connect('gerenciador_tarefas_db.sqlite3')
+cursor = conexao.cursor()
+
+sql = """   CREATE TABLE categorias (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL
+                ); """
+
+cursor.execute(sql)
+conexao.commit
+
+sql = """    CREATE TABLE tarefas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                categoria_id INTEGER NOT NULL,
+                data DATE NOT NULL,
+                realizada INTEGER NOT NULL,
+                FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+                ); """
+
+cursor.execute(sql)
+conexao.commit
+
+sql = """    CREATE TABLE status (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL
+                );
+      """
+
+cursor.execute(sql)
+conexao.commit
+conexao.close()
